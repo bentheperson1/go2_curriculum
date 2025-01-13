b7f4670d02433e01364905c441c83e38bff5127c
Ben Holland <bentheperson1@gmail.com>
2025-01-13 14:52:48 -0600

    lib

--- example.py
+++ example.py
@@ -11,21 +11,21 @@ async def main():
 
     dog.video_display_stream(1280, 720)
 
-    await dog.audio_set_volume(10)
-    await dog.light_set_color(VUI_COLOR.RED)
-    await dog.motion_perform_normal_action(NormalActions.HELLO)
+    # await dog.audio_set_volume(10)
+    # await dog.light_set_color(VUI_COLOR.RED)
+    # await dog.motion_perform_normal_action(NormalActions.HELLO)
 
-    await dog.motion_move(1, 0, 0)
-    await dog.motion_move(-1, 0, 0)
+    # await dog.motion_move(1, 0, 0)
+    # await dog.motion_move(-1, 0, 0)
 
-    await dog.motion_perform_normal_action(NormalActions.HEART)
+    # await dog.motion_perform_normal_action(NormalActions.HEART)
 
-    await dog.motion_switch_mode(MotionState.AI)
-    await dog.motion_perform_ai_action(AIActions.HANDSTAND, True)
+    # await dog.motion_switch_mode(MotionState.AI)
+    # await dog.motion_perform_ai_action(AIActions.HANDSTAND, True)
 
-    await asyncio.sleep(5)
+    # await asyncio.sleep(5)
 
-    await dog.motion_perform_ai_action(AIActions.HANDSTAND, False)
+    # await dog.motion_perform_ai_action(AIActions.HANDSTAND, False)
 
     dog.audio_play_mp3_from_file("audio/unicorn.mp3")
 
--- go2_module.py
+++ go2_module.py
@@ -43,7 +43,7 @@ class AIActions:
 
 class RobotDog:
 	def __init__(self):
-		self.conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, serialNumber="B42D2000XXXXXXXX")
+		self.conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalAP, serialNumber="B42D2000XXXXXXXX")
 		self.frame_queue = Queue()
 		self.window_title = "Dog Video"
 		self.current_motion_switcher_mode = ""
